3301ae1089225d8c5034195ba2a585dedb410f07
Maciej <macwal1234@gmail.com>
2022-04-05 12:11:23 +0200

    security bundle(registration form and registration site)

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,6 +1,10 @@
 <?php
+
 namespace App\Controller;
+
+use App\Entity\User;
 use App\Form\BlogPostFormType;
+use App\Form\RegistrationFormType;
 use Symfony\Component\Form\FormTypeInterface;
 use App\Entity\BlogCategory;
 use App\Entity\BlogPost;
@@ -10,104 +14,182 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\CategoryformType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class BlogController extends  AbstractController
+class BlogController extends AbstractController
 {
-
-    public function view(ManagerRegistry $doctrine,int $id): Response
+    /*
+     * Show full list of posts based on category or all of them
+     */
+    public function view(ManagerRegistry $doctrine, int $id): Response
     {
-        if($id != 0 ) {
+        if ($id != 0) {
             $category = $doctrine->getRepository(BlogCategory::class)->find($id);
             $posts = $category->getPost();
-        }
-        else{
+        } else {
             $posts = $doctrine->getRepository(BlogPost::class)->findAll();
         }
-        return $this->render('blog/view.html.twig',[
-            "posts"=>$posts,
+        return $this->render('blog/view.html.twig', [
+            "posts" => $posts,
         ]);
     }
-    public function showpost(ManagerRegistry $doctrine,int $id): Response
+
+    /*
+     * Show blog post in detail
+     */
+    public function showpost(ManagerRegistry $doctrine, int $id): Response
     {
-        $post= $doctrine->getRepository(BlogPost::class)->find($id);
-        return $this->render('blog/showpost.html.twig',[
-            "post"=>$post,
+        $post = $doctrine->getRepository(BlogPost::class)->find($id);
+        return $this->render('blog/showpost.html.twig', [
+            "post" => $post,
         ]);
     }
+
+    /*
+     * Base homepage
+     */
     public function index(): Response
     {
         $name = "Homepage";
-        return $this->render('blog/index.html.twig',[
-            "name"=>$name,
+        return $this->render('blog/index.html.twig', [
+            "name" => $name,
         ]);
     }
+
+    /*
+     * Show list of categories as well as buttons to add category and blog post
+     */
     public function list(ManagerRegistry $doctrine): Response
     {
         $repository = $doctrine->getRepository(BlogCategory::class);
         $blogCategories = $repository->findAll();
 
-        return $this->render('blog/list.html.twig',[
-            "blogCategories"=>$blogCategories,
+        return $this->render('blog/list.html.twig', [
+            "blogCategories" => $blogCategories,
         ]);
     }
-    public function newcat(Request $request,ManagerRegistry $doctrine)
+
+    /*
+     * Add new category.
+     * Redirect to list and show notice if valid
+     * Show error message if not valid
+     */
+    public function newcat(Request $request, ManagerRegistry $doctrine)
     {
         $category = new BlogCategory();
 
         $form = $this->createForm(CategoryformType::class, $category);
         $form->handleRequest($request);
 
-        if($form->isSubmitted()&&$form->isValid())
-        {
-            $this->addFlash(
-                'notice',
-                'Your changes were saved!'
-            );
-            $em = $doctrine->getManager();
-            $em->persist($category);
-            $em->flush();
-            return $this->redirectToRoute('newcat');
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $this->addFlash(
+                    'notice',
+                    'You created your category congratz!'
+                );
+                $em = $doctrine->getManager();
+                $em->persist($category);
+                $em->flush();
+                return $this->redirectToRoute('list');
+            } else {
+                $this->addFlash(
+                    'error',
+                    'Something went wrong sorry!'
+                );
+            }
         }
-
-        return $this->render('blog/newcat.html.twig',[
-            'form'=>$form->createView(),
+        return $this->render('blog/newcat.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
-    public function newpost(Request $request,ManagerRegistry $doctrine): Response
-    {
-    $post = new BlogPost();
 
+    /*
+        * Add new Post.
+        * Redirect to list and show notice if valid
+        * Show error message if not valid
+        */
+    public function newpost(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $post = new BlogPost();
         $form = $this->createForm(BlogPostFormType::class, $post);
         $form->handleRequest($request);
-        if($form->isSubmitted()&&$form->isValid())
-        {
-            $this->addFlash(
-                'notice',
-                'Your changes were saved!'
-            );
-            $em = $doctrine->getManager();
-            $em->persist($post);
-            $em->flush();
-            return $this->redirectToRoute('newpost');
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $this->addFlash(
+                    'notice',
+                    'You created your post congratz!'
+                );
+                $em = $doctrine->getManager();
+                $em->persist($post);
+                $em->flush();
+                return $this->redirectToRoute('list');
+            } else {
+                $this->addFlash(
+                    'error',
+                    'Something went wrong sorry!'
+                );
+            }
         }
-    return $this->render('blog/newpost.html.twig',[
-        'form'=>$form->createView(),
-    ]);
-}
+        return $this->render('blog/newpost.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 
+    /*
+     * Show login page.
+     */
     public function login(): Response
     {
         $name = "Login";
-        return $this->render('blog/login.html.twig',[
-            "name"=>$name,
-        ]);
-    }
-    public function contact(): Response
-    {
-        $name = "Contact";
-        return $this->render('blog/contact.html.twig',[
-            "name"=>$name,
+        return $this->render('blog/login.html.twig', [
+            "name" => $name,
         ]);
     }
 
+    /*
+     * Show contact page
+     */
+    public function contact(): Response
+    {
+        $name = "Contact";
+        return $this->render('blog/contact.html.twig', [
+            "name" => $name,
+        ]);
+    }
+
+    /*
+     * Registration
+     */
+    public function register(UserPasswordHasherInterface $passwordHasher,Request $request, ManagerRegistry $doctrine)
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $plainPassword = $form->get('plainPassword')->getData();
+                $hashedPassword = $passwordHasher->hashPassword(
+                    $user,
+                    $plainPassword
+                );
+                $user->setPassword($hashedPassword);
+                $this->addFlash(
+                    'notice',
+                    'Welcome new user!'
+                );
+                $em = $doctrine->getManager();
+                $em->persist($user);
+                $em->flush();
+                return $this->redirectToRoute('list');
+            } else {
+                $this->addFlash(
+                    'error',
+                    'Something went wrong sorry!'
+                );
+            }
+        }
+        return $this->render('blog/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 }
